ac3ec3e4d5fae05bf8d7cfbcfc57b0025c4fa9fe
sunleecat <916321256@qq.com>
2017-03-22 20:10:48 +0800

    first

---
+++ app/src/main/java/com/example/sunleeweather/db/County.java
@@ -0,0 +1,45 @@
+package com.example.sunleeweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/3/22.
+ */
+
+public class County extends DataSupport {
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getCountyName(){
+        return countyName;
+    }
+    public void setCountyName(String countName){
+        this.countyName=countName;
+    }
+    public String  getWeatherId(){
+        return weatherId;
+    }
+    public void setWeatherId(String weatherId){
+        this.weatherId=weatherId;
+    }
+    public int getCityIdId(){
+        return cityId;
+    }
+
+    public void setCityId(int cityId){
+        this.cityId=cityId;
+    }
+}
